9d9e457dda4ee3cf2019052124a3160213cbe058
Hethos20 <teenmonkey123@gmail.com>
2020-03-08 00:39:23 -0500

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -12,34 +12,25 @@ cp -ar $HOME/i3-Setup/home/.config/* $HOME/.config
 cp -ar $HOME/i3-Setup/home/.scripts/* $HOME/.scripts
 cp -a $HOME/i3-Setup/home/{.bashrc,.profile,.vimrc,.Xresources} $HOME
 
-echo "1.1: installing i3 blocks" | tee -a $ERFile
-cd $HOME/.programs
-git clone https://github.com/Airblader/i3blocks-gaps i3blocks >> $ERFile 2>&1
-cd i3blocks
-sudo make clean debug >> $ERFile 2>&1
-sudo make install >> $ERFile 2>&1
-#rm $HOME/.programs/i3blocks/scripts/volume
-#cp -r $HOME/i3-Setup/volume $HOME/.programs/i3blocks/scripts
-
 cd $HOME/.programs
 
-echo "1.2: installing Firefox-nightly" | tee -a $ERFile
+echo "1.1: installing Firefox-nightly" | tee -a $ERFile
 wget "https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-US" -O firefox.tar.bz2 >> $ERFile 2>&1
 tar -xvf firefox.tar.bz2 >> $ERFile 2>&1
 rm firefox.tar.bz2
 
-echo "1.3: installing Bitwarden" | tee -a $ERFile
+echo "1.2: installing Bitwarden" | tee -a $ERFile
 wget "https://vault.bitwarden.com/download/?app=desktop&platform=linux" -O bitwarden.AppImage >> $ERFile 2>&1
 sudo chmod +x bitwarden.AppImage
 
-echo "1.4: installing Dmenu" | tee -a $ERFile
+echo "1.3: installing Dmenu" | tee -a $ERFile
 git clone https://git.suckless.org/dmenu >> $ERFile 2>&1
 sudo apt-get install libx11-dev libxinerama1 libxinerama-dev libxft-dev -yy >> $ERFile 2>&1
 sudo make clean install >> $ERFile 2>&1
 
 cd $HOME/.games
 
-echo "1.5: installing Artix launcher" | tee -a $ERFile
+echo "1.4: installing Artix launcher" | tee -a $ERFile
 wget "https://launch.artix.com/latest/Artix_Games_Launcher-x86_64.AppImage" -O artix.AppImage >> $ERFile 2>&1
 sudo chmod +x artix.AppImage
 
