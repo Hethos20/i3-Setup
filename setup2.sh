8f0ab5000aa301fa0fb0383210b10394cda8cd24
Hethos20 <teenmonkey123@gmail.com>
2020-03-10 01:17:55 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -154,8 +154,9 @@ sudo apt-get install synaptic -yy >> $ERFile 2>&1
 echo "1.34: installing syncthing" | tee -a $ERFile
 sudo apt-get install syncthing -yy >> $ERFile 2>&1
 
-
 echo "1.35: installing mocp" | tee -a $ERFile
 sudo apt-get install mocp -yy >> $ERFile 2>&1
+touch $HOME/.moc/config
+echo "Theme = transparent-background" > $HOME/.moc/config
 
 reboot
